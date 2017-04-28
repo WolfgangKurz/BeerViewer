32c092bee9a00cca818c57ce5dd7f113252380db
WolfgangKurz <wolfgangkurzdev@gmail.com>
2017-04-28 21:03:25 +0900

    port Done

--- BeerViewer/Models/Raw/Extensions.cs
+++ BeerViewer/Models/Raw/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using BeerViewer.Models.Enums;
 
@@ -58,6 +59,27 @@ namespace BeerViewer.Models.Raw
 				default:
 					return false;
 			}
+		}
+
+		public static IEnumerable<TResult> Return<TResult>(TResult value)
+		{
+			yield return value;
+		}
+
+		public static IEnumerable<TSource> Distinct<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+		{
+			var set = new HashSet<TKey>(EqualityComparer<TKey>.Default);
+
+			foreach (var item in source)
+			{
+				var key = keySelector(item);
+				if (set.Add(key)) yield return item;
+			}
+		}
+
+		public static bool HasItems<T>(this IEnumerable<T> source)
+		{
+			return source != null && source.Any();
 		}
 	}
 }
