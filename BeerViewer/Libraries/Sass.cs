e3293797c294e6018e1c0d56e1d742bca28eda23
WolfgangKurz <wolfgangkurzdev@gmail.com>
2019-01-02 19:09:55 +0900

    Changing into SASS(SCSS)

---
+++ BeerViewer/Libraries/Sass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NSass;
+
+namespace BeerViewer.Libraries
+{
+	public class Sass
+	{
+		public static Sass Instance { get; } = new Sass();
+
+		public void Compile()
+		{
+		}
+	}
+}
