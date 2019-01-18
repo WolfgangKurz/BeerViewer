58dfa66b2c2fd94b943b9598e2c8bba839528b82
WolfgangKurz <wolfgangkurzdev@gmail.com>
2019-01-18 18:57:31 +0900

    Merge "Exports/API" files.

--- WindowFrame/source/System/Exports/API.ts
+++ WindowFrame/source/System/Exports/API.ts
@@ -1,9 +1,6 @@
-export type HTTPCallback = (Response: string, Request: HTTPRequest) => void;
+import { IDisposable } from "../Base/Interfaces/IDisposable";
 
-export class HTTPRequest {
-    [key: string]: string | string[] | number;
-    constructor(data: any);
-}
+export type HTTPCallback = (Response: string, Request: HTTPRequest) => void;
 
 declare global {
     export interface Window {
@@ -73,4 +70,35 @@ export interface ModuleInfo {
 
     /** *.css exists? */
     Styled: boolean;
+}
+
+/** Subscribe information */
+export class SubscribeInfo implements IDisposable {
+    private _Id: number;
+    public get Id(): number { return this._Id }
+
+    constructor(Id: number) {
+        this._Id = Id;
+    }
+
+    Dispose(): void {
+        if (this.Id >= 0)
+            window.API.UnsubscribeHTTP(this.Id);
+    }
+}
+
+export class HTTPRequest {
+    [key: string]: string[] | string | number;
+
+    constructor(data: any) {
+        if (data === undefined || data === null) return;
+
+        for (let key in data) {
+            const value = data[key], nvalue = Number(value);
+            if (nvalue.toString() === value)
+                this[key] = nvalue; // Real numeric
+            else
+                this[key] = value;
+        }
+    }
 }
